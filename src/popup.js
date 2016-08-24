05ebf896b8a43c4ba26c78a9b300b53a88ed0ff7
Lance <Lance@lancedeMacBook-Pro.local>
2016-08-24 20:10:34 +0800

    update

--- src/popup.js
+++ src/popup.js
@@ -5,10 +5,16 @@ function Popup() {
 }
 
 Popup.prototype = {
-    alert: function (content) {
+    alert: function (content, handler) {
         const popupBox = $("<div class='popup_window'></div>");
-        popupBox.appendTo('body');
+        const btn = $("<input type='button' value='确定'/>");
         popupBox.html(content);
+        btn.click(function () {
+            handler && handler();
+            popupBox.remove();
+        });
+        btn.appendTo(popupBox);
+        popupBox.appendTo('body');
     },
     confirm: function () {
 
